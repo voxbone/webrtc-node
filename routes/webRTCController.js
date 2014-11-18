cc9d5c475a6663bd7ab41a7e62a04260f262616b
Vincent Morsiani <vmorsiani@gmail.com>
2014-11-18 11:03:39 +0100

    Update webRTCController.js
    removing some logging

--- routes/webRTCController.js
+++ routes/webRTCController.js
@@ -17,8 +17,6 @@ var WebRtcController = function () {
     self.createKey = function (user) {
         self.username = configuration.voxbone.webrtc.default_username;
         self.secret = configuration.voxbone.webrtc.default_secret;
-        console.log(configuration);
-        console.log('Generating key for user %s/%s', self.username, self.secret);
         var hmac = crypto.createHmac('sha1', self.secret);
         self.expires = Math.round(Date.now()/1000) + 300;
         var text = self.expires + ':' + self.username;
